e6704f6a0e2d36e739f059c268d5e56e6ea774d6
505856727 <30371192+505856727@users.noreply.github.com>
2017-08-10 22:46:10 +0800

    jiaer

--- Assets/jiaer_ceshi/huadong.cs
+++ Assets/jiaer_ceshi/huadong.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class huadong : MonoBehaviour {
+public class huadong : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
+    /**
+     * 判断是否为单点触摸
+     **/
+    public static bool singleTouch()
+    {
+        if (Input.touchCount == 1)
+            return true;
+        return false;
+    }
+
+    /**
+     * 判断单点触摸条件下  是否为移动触摸
+     **/
+    public static bool moveSingleTouch()
+    {
+        if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            return true;
+        return false;
+    }
+
+    public Vector3 fangxiang(Vector3 begin, Vector3 end)
+    {
+        while (moveSingleTouch())
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                begin = Input.GetTouch(0).position;
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                end = Input.GetTouch(0).position;
+            }           
+        }
+        return end - begin;
+    }
 }
