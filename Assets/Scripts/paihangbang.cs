4bea9a88521a8aa3212cff6aa566a2f4ed19eabc
505856727 <30371192+505856727@users.noreply.github.com>
2017-08-10 22:11:35 +0800

    加二
    实验一下

---
+++ Assets/Scripts/paihangbang.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paihangbang : MonoBehaviour
+{
+    public paihang a = new paihang();
+    void Awake()
+    {
+        
+    }
+}
