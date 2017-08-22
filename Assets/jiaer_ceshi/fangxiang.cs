5c3e507122b463d51a335f5c1a5c0a6adc01a338
xiaomu1993 <30407753+xiaomu1993@users.noreply.github.com>
2017-08-22 22:20:39 +0800

    小修改

--- Assets/jiaer_ceshi/fangxiang.cs
+++ Assets/jiaer_ceshi/fangxiang.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class fangxiang : MonoBehaviour {
-    public Vector3 position1;
-    public Vector3 position2;
-    Vector3  fxld()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            position1 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
-        if (Input.GetMouseButton(0))
-        {
-            position2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
-        while ((position2 - position1).magnitude == 14.1F)
-        {
-            Vector3 lidufx = (position2 - position1).normalized;
-        }
-    }
+    //public Vector3 position1;
+    //public Vector3 position2;
+    //Vector3  fxld()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        position1 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //    }
+    //    if (Input.GetMouseButton(0))
+    //    {
+    //        position2 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //    }
+    //    while ((position2 - position1).magnitude == 14.1F)
+    //    {
+    //        Vector3 lidufx = (position2 - position1).normalized;
+    //    }
+    //}
 }
