0a0da16af185a8164f18d4273ef79ea93a8f9361
505856727 <30371192+505856727@users.noreply.github.com>
2017-08-26 12:12:10 +0800

    jiaer

--- Assets/Scripts/zhu_jue_pz.cs
+++ Assets/Scripts/zhu_jue_pz.cs
@@ -219,7 +219,10 @@ public class zhu_jue_pz : MonoBehaviour {
 
     void qian_jing()//推动主角前进的力
     {
-        rigidbody2D.AddForce(new Vector2(1, 0)*qsu_du);//修改成负数了 20170805 我又改正数了
+        
+            rigidbody2D.AddForce(new Vector2(1, 0)*qsu_du);//修改成负数了 20170805 我又改正数了
+        
+       
     }
     
     void xu_li()//弹跳蓄力
